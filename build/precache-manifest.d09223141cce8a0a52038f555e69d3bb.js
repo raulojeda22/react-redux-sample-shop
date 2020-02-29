7616aff94847dded6fc66ece5f1f4d1304d501db
raulojeda22 <raulojeda10g@gmail.com>
2020-02-29 15:56:09 +0100

    add build

---
+++ build/precache-manifest.d09223141cce8a0a52038f555e69d3bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63fb80e08c6ebce1e32ae254b6c6ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f8772cdfc218d2936ce",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "b7a2f11d1bf79672f957",
+    "url": "/static/css/main.91fab133.chunk.css"
+  },
+  {
+    "revision": "1f8772cdfc218d2936ce",
+    "url": "/static/js/2.7a0507a0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.7a0507a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a2f11d1bf79672f957",
+    "url": "/static/js/main.f767b90f.chunk.js"
+  },
+  {
+    "revision": "c655922743b8535e90ff",
+    "url": "/static/js/runtime-main.67b15af4.js"
+  }
+]);
